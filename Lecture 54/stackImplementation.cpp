1ce695864d8a68837efb689886d628cc41c981ba
Abhishek Kumar <abhixsliet@gmail.com>
2023-07-14 15:17:12 +0530

    Stack Section Added

--- Lecture 54/stackImplementation.cpp
+++ Lecture 54/stackImplementation.cpp
@@ -2,43 +2,6 @@
 #include<stack>
 using namespace std;
 
-/* 
-int main()
-{
-    //creation of stack using STL
-    stack<int>s;
-    
-    //push operation
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-    s.push(50);
-
-    //pop operation
-    s.pop();
-    s.pop();
-    s.pop();
-
-    cout << "Top element of the stack => " << s.top() << endl;
-
-    if(s.empty())
-    {
-        cout << "Element is not present in stack" << endl;
-    }
-    else
-    {
-        cout << "Element is present in stack" << endl;
-    }
-
-    cout << "Size of stack => " << s.size() << endl;
-
-    return 0;
-}
-*/
-
-
-
 class Stack
 {
     //properties
@@ -69,7 +32,7 @@ class Stack
         }
     }
 
-    //Here we are including index-0 
+    //other way
     // void push(int val)
     // {
     //     if(top >= size)
@@ -149,138 +112,8 @@ int main()
     {
         cout << "Stack is not empty" << endl;
     }
+
+
+    cout << endl;
+    return 0;
 }
-
-
-/* 
-// Stack Implementation using linked list
-
-class Node
-{
-    public:
-    int data;
-    Node* link;
-
-    Node(int d)
-    {
-        this -> data = d;
-        this -> link = NULL;
-
-    }
-
-};
-
-class Stack
-{
-    Node* top;
-    public:
-        Stack()
-        {
-            top = NULL;
-        }
-
-        
-        void push(int element)
-        {
-            //creating a node & allocating memory dynamically
-            Node* newNode = new Node(element);
-            
-            //checking stack has space or not
-            if(!newNode)
-            {
-                cout << "Stack Overflow" << endl;
-            }
-            
-            newNode -> data = element;
-            newNode -> link = top;
-            top = newNode;
-            
-        }
-
-        void pop()
-        {
-            Node* newNode;
-
-            //checking stack has element or not
-            if(top == NULL)
-            {
-                cout << "Stack Underflow" << endl;
-            }
-            else
-            {
-                newNode = top;
-                top = top -> link;
-                delete newNode ;
-            }
-        }
-
-        bool isEmpty()
-        {
-            return top == NULL;
-        }
-
-        int peek()
-        {
-            if(!isEmpty())
-            {
-                return top -> data;
-            }
-            else
-            {
-                cout << "Stack is empty" << endl;
-                return -1;
-            }
-        }
-
-        void display()
-        {
-            Node* temp ;
-            if(temp == NULL)
-            {
-                cout << "Stack underflow" << endl;
-            }
-            else
-            {
-                temp = top;
-                while(temp != NULL)
-                {
-                    cout << temp -> data << " -> " ;
-                    temp = temp -> link;
-                }cout << endl;
-            }
-        }
-};
-
-int main()
-{
-    Stack st;
-    st.push(10);
-    st.push(20);
-    st.push(30);
-    st.push(40);
-    st.push(50);
-    st.display();
-    cout << "Top element of stack is => " << st.peek() << endl;
-
-    st.pop();
-    st.pop();
-    st.pop();
-    st.display();
-    cout << "Top element of stack is => " << st.peek() << endl;
-
-    if(st.isEmpty())
-    {
-        cout << "Stack khali hai mere bhaai" << endl;
-    }
-    else
-    {
-        cout << "Stack is not khali mere bhaai" << endl;
-    }
-
-}
-
- */
-
-
-//Did question on coding ninja
-//Two stack problem using an array
