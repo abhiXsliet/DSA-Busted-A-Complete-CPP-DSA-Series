5aaa190b2bbe34c04a768b00ff89251dbc85c2e7
Abhishek Kumar <abhixsliet@gmail.com>
2023-07-23 18:40:12 +0530

    Linked List(Doubly LL) Section Modified

--- Lecture 44 Linked List Day 01/doublyLL.cpp
+++ Lecture 44 Linked List Day 01/doublyLL.cpp
@@ -159,7 +159,7 @@ void print(Node *head)
     Node *temp = head;
     while (temp != NULL)
     {
-        cout << temp -> data << " -> ";
+        cout << temp -> data << " <-> ";
         temp = temp -> next;
     }
     cout << endl;
