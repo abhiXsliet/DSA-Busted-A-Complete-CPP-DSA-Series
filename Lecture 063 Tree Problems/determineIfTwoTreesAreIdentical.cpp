c6ada191f77303adedaf2c9f91134e3066415771
Abhishek Kumar <abhixsliet@gmail.com>
2024-02-28 00:13:14 +0530

    Binary Tree Section Updated

--- Lecture 063 Tree Problems/determineIfTwoTreesAreIdentical.cpp
+++ Lecture 063 Tree Problems/determineIfTwoTreesAreIdentical.cpp
@@ -3,93 +3,26 @@
 
 
 //{ Driver Code Starts
-//Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
-
-// Tree Node
-struct Node {
+struct Node
+{
     int data;
-    Node* left;
-    Node* right;
-};
+    struct Node *left;
+    struct Node *right;
 
-// Utility function to create a new Tree Node
-Node* newNode(int val) {
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-
-    return temp;
-}
-
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N') return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;) ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size()) break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
     }
-
-    return root;
-}
+};
 
 
 // } Driver Code Ends
-/* A binary tree node structure
+/* A binary tree node
+
 
 struct Node
 {
@@ -102,65 +35,108 @@ struct Node
         left = right = NULL;
     }
 };
- */
-
-class Solution{
-    private:
-    pair<bool, int> isBalancedFast(Node* root){
-        //base case
-        if(root == NULL){
-            pair<bool, int> p = make_pair(true, 0);
-            return p;
-        }
-        
-        pair<int, int> left = isBalancedFast(root -> left);
-        pair<int, int> right = isBalancedFast(root -> right);
-        
-        bool leftAns = left.first;
-        bool rightAns = right.first;
-        
-        bool diff = abs (left.second - right.second) <= 1; 
-        
-        pair<bool, int> ans;
-        ans.second = max(left.second, right.second) + 1;
-        
-        if(leftAns && rightAns && diff) {
-            ans.first = true;
-        }
-        else {
-            ans.first = false;
-        }
-        
-        return ans;
-        
-    }
-    
+*/
+class Solution
+{
     public:
-    //Function to check whether a binary tree is balanced or not.
-    bool isBalanced(Node *root)
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
     {
-         return isBalancedFast(root).first;
+        if (r1 == NULL && r2 == NULL) return true;
+        if (r1 != NULL && r2 == NULL) return false;
+        if (r1 == NULL && r2 != NULL) return false;
+        
+        bool left  = isIdentical(r1 -> left , r2 -> left);
+        bool right = isIdentical(r1 -> right, r2 -> right);
+        
+        bool value = (r1 -> data == r2 -> data);
+        
+        if (left && right && value) return true;
+        else return false;
     }
 };
 
-
 //{ Driver Code Starts.
-/* Driver program to test size function*/
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
 
 int main() {
-
-   
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-        string s, ch;
-        getline(cin, s);
-        
-        Node* root = buildTree(s);
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str, str1;
+        getline(cin, str);
+        Node *rootA = buildTree(str);
+        getline(cin, str1);
+        Node *rootB = buildTree(str1);
         Solution ob;
-        cout << ob.isBalanced(root) << endl;
+        if (ob.isIdentical(rootA, rootB)) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
     }
     return 0;
 }
-
 // } Driver Code Ends
